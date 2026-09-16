c894d892f99725a100ad61d79aa8b54faca63111
Ritika Kumari Gupta <259775213+ritikakumarigupta@users.noreply.github.com>
2026-09-16 23:10:25 +0530

    Update next config

---
+++ next.config.ts
@@ -0,0 +1,15 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  images: {
+    unoptimized: true,
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '500mb',
+    },
+  },
+};
+
+export default nextConfig;
